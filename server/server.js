ed95defe7d2784f4e7b59a6347a15674c70a3b39
Pranjal Srivastva <pranjalsri.0309@gmail.com>
2026-09-20 11:24:29 +0530

    chore: configure environment

--- server/server.js
+++ server/server.js
@@ -1,17 +1,20 @@
+import dotenv from 'dotenv';
 import express from 'express';
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
 app.get('/health', (req, res) => {
-  res.json({
-    message: 'Server is running!',
-  });
+    res.json({
+        message: 'Server is running!',
+    });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });

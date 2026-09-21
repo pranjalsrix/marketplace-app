c5655f27d1bcf56b97bbd376a03e19ff7abff123
Pranjal Srivastva <pranjalsri.0309@gmail.com>
2026-09-21 11:57:03 +0530

    feat: add user registration

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import connectDB from "./config/db.js";
-import userRoutes from "./routes/userRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
 
 app.get('/health', (req, res) => {
     res.json({
